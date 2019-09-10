9064dfc0d7f3f35927f07fcfdaa387c2a6e020e6
surkov-m <surkov-m@yandex-team.ru>
2019-09-10 18:47:00 +0300

    add task2

--- hw01/src/knn.py
+++ hw01/src/knn.py
@@ -7,19 +7,27 @@ import pandas
 import typing
 
 
-def read_cancer_dataset(path_to_csv):
+def read_cancer_dataset(path_to_csv: str) -> typing.Tuple[typing.List, typing.List]:
     data = pandas.read_csv(path_to_csv).values.tolist()
     random.shuffle(data)
     return [row[1:] for row in data], [row[0] for row in data]
 
 
-def read_spam_dataset(path_to_csv):
+def read_spam_dataset(path_to_csv: str) -> typing.Tuple[typing.List, typing.List]:
     data = pandas.read_csv(path_to_csv).values.tolist()
     random.shuffle(data)
     return [row[:-1] for row in data], [int(row[-1]) for row in data]
 
 
+def train_test_split(
+        X: typing.List,
+        y: typing.List,
+        ratio: float
+) -> typing.Tuple[typing.List, typing.List, typing.List, typing.List]:
+    assert len(X) == len(y)
+    train_size = int(ratio * len(X))
+    return X[:train_size], y[:train_size], X[train_size:], y[train_size:]
+
+
 # X, y = read_cancer_dataset('resources/cancer.csv')
 # X, y = read_spam_dataset('resources/spam.csv')
-
-
