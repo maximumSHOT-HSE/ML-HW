db385df5dd5d8fdb16596eddde6ece5b54d38851
surkov-m <surkov-m@yandex-team.ru>
2019-09-11 17:53:00 +0300

    refactor files

--- hw01/src/main/KDTree.py
+++ hw01/src/main/KDTree.py
@@ -46,6 +46,13 @@ class Node:
         self.box_center = (down_boundary + up_boundary) / 2
         self.box_radius = get_dist(self.box_center, down_boundary)
 
+    # def check_intersection(self, center: np.ndarray, radius: float) -> bool:
+    #     r1 = np.array([self.box_radius] * self.box_center.shape[0])
+    #     r2 = np.array([radius] * center.shape[0])
+    #     x = np.array([self.box_center - r1, center - r2]).max(axis=0)
+    #     y = np.array([self.box_center + r1, center + r2]).min(axis=0)
+    #     return (x <= y).all()
+
     def check_intersection(self, center: np.ndarray, radius: float) -> bool:
         return get_dist(self.box_center, center) <= self.box_radius + radius
 
@@ -126,3 +133,52 @@ class KDTree:
             return [[(p.id, p.get_dist(xs[i])) for p in knn] for i, knn in enumerate(result)]
         else:
             return [[p.id for p in knn] for knn in result]
+
+
+def check_correctness():
+
+    def true_closest(xx_train, xx_test, k):
+        result = []
+        for x0 in xx_test:
+            bests = list(sorted([(i, np.linalg.norm(x - x0)) for i, x in enumerate(xx_train)], key=lambda x: x[1]))
+            bests = [i for i, d in bests]
+            result.append(bests[:min(k, len(bests))])
+        return result
+
+    x_train = np.random.randn(100, 20)
+    x_test = np.random.randn(100, 20)
+    tree = KDTree(x_train, leaf_size=2)
+    predicted = tree.query(x_test, k=4, return_distance=False)
+    true = true_closest(x_train, x_test, k=4)
+
+    predicted = [list(sorted(l)) for l in predicted]
+    true = [list(sorted(l)) for l in true]
+
+    print(np.array(true))
+    print('===================')
+    print(np.array(predicted))
+
+    if np.sum(np.abs(np.array(np.array(predicted).shape) - np.array(np.array(true).shape))) != 0:
+        print("Wrong shape")
+    else:
+        errors = sum([1 for row1, row2 in zip(predicted, true) for i1, i2 in zip(row1, row2) if i1 != i2])
+        if errors > 0:
+            print(errors, "errors")
+        else:
+            print('OK')
+
+
+def execute_max_test():
+    x_train = np.random.randn(1000, 20)
+    x_test = np.random.randn(1000, 20)
+    tree = KDTree(x_train, leaf_size=2)
+    # predicted = true_closest(x_train, x_test, k=4)
+    predicted = tree.query(x_test, k=4)
+
+    predicted = [list(sorted(l)) for l in predicted]
+
+    print(np.array(predicted))
+
+
+# check_correctness()
+# execute_max_test()
