42df09a14b39b38dce14dd100829d739a9c7b4ed
Maxim Surkov <42948730+maximumSHOT-HSE@users.noreply.github.com>
2019-09-23 12:52:50 +0300

    Hw01 (#1)
    * fix gitignore
    
    * fix gitignore
    
    * add task1
    
    * add resources into the gitignore
    
    * add task2
    
    * add task3
    
    * add resources
    
    * add basics for kd tree
    
    * before testing
    
    * check tests
    
    * refactor files
    
    * add task 5
    
    * refactor files structure
    
    * move code into the separate package
    
    * fix bug realted with equal points
    
    * add results
    
    * finish
    
    * fix bug
    
    * add code to the notebook
    
    * add scales to utils
    
    * fix min max scale
    
    * fix max abs scale
    
    * fix scales
    
    * fix scales
    
    * add scale to notebook
    
    * remove useless method run
    
    * finally, add scales to notebook
    
    * add scales for spam dataset

---
+++ hw01/src/KDTree.py
@@ -0,0 +1,133 @@
+import typing
+
+import numpy as np
+
+
+def get_dist(a: np.ndarray, b: np.ndarray):
+    return np.linalg.norm(a - b)
+
+
+class Point:
+
+    def __init__(self, vector, i):
+        self.vector = vector
+        self.id = i
+
+    def get_dist(self, o: np.ndarray):
+        return get_dist(self.vector, o)
+
+
+class Node:
+
+    def __init__(
+            self,
+            axis: int,
+            axis_value: float,
+            points_number: int,
+            left_son=None,
+            right_son=None,
+            points: typing.List[Point] = None
+    ):
+        self.axis = axis
+        self.axis_value = axis_value
+        self.points_number = points_number
+        self.points = points
+        self.left_son = left_son
+        self.right_son = right_son
+
+        self.box_center = np.array([])
+        self.box_radius = 0
+
+        self.build_boundaries()
+
+    def build_boundaries(self):
+        up_boundary = np.array([p.vector for p in self.points]).max(axis=0)
+        down_boundary = np.array([p.vector for p in self.points]).min(axis=0)
+        self.box_center = (down_boundary + up_boundary) / 2
+        self.box_radius = get_dist(self.box_center, down_boundary)
+
+    def check_intersection(self, center: np.ndarray, radius: float) -> bool:
+        return get_dist(self.box_center, center) <= self.box_radius + radius
+
+
+class KDTree:
+
+    def build_tree(
+            self,
+            points: typing.List[Point],
+            axis: int,
+            dimension: int,
+            leaf_size: int,
+            depth: int = 0
+    ) -> typing.Optional[Node]:
+        if len(points) == 0:
+            return None
+        pivot = float(np.median(np.array([p.vector[axis] for p in points])))
+        points_number = len(points)
+
+        if points_number <= leaf_size:
+            return Node(axis, pivot, points_number, points=points)
+
+        points = [p for p in points if p.vector[axis] < pivot] +\
+                 [p for p in points if p.vector[axis] == pivot] +\
+                 [p for p in points if p.vector[axis] > pivot]
+
+        left_part = points[:points_number // 2]
+        right_part = points[points_number // 2:]
+
+        left_son = self.build_tree(left_part, (axis + 1) % dimension, dimension, leaf_size, depth + 1)
+        right_son = self.build_tree(right_part, (axis + 1) % dimension, dimension, leaf_size, depth + 1)
+
+        return Node(axis, pivot, points_number, left_son, right_son, points)
+
+    def __init__(self, points: np.ndarray, leaf_size: int = 40):
+        points = [Point(vector, i) for i, vector in enumerate(points)]
+        points_number = len(points)
+        assert points_number > 0
+        dimension = points[0].vector.shape[0]
+        assert dimension > 0
+        self.root = self.build_tree(points, 0, dimension, leaf_size)
+
+    def find_circle_boundary(self, point: np.ndarray, node: Node, k: int):
+        node_to = node.left_son if point[node.axis] < node.axis_value else node.right_son
+        if node_to and node_to.points_number >= k:
+            return self.find_circle_boundary(point, node_to, k)
+        node.points.sort(key=lambda p: p.get_dist(point))
+        return node.points[min(k, len(node.points)) - 1].get_dist(point), node.points[:min(k, len(node.points))]
+
+    def update_knn(
+            self,
+            node: Node,
+            current_knn: typing.List[Point],
+            k: int,
+            center: np.ndarray,
+            radius: float
+    ) -> typing.Tuple[float, typing.List[Point]]:
+        if not node or not node.check_intersection(center, radius):
+            return radius, current_knn
+        if not node.left_son and not node.right_son:
+            current_knn += node.points
+            current_knn.sort(key=lambda p: p.get_dist(center))
+            if k < len(current_knn):
+                current_knn = current_knn[:k]
+            radius = current_knn[-1].get_dist(center)
+            return radius, current_knn
+        radius, current_knn = self.update_knn(node.left_son, current_knn, k, center, radius)
+        radius, current_knn = self.update_knn(node.right_son, current_knn, k, center, radius)
+        return radius, current_knn
+
+    def find_knn(self, point: np.ndarray, k: int) -> typing.List[Point]:
+        assert self.root.points_number >= k
+        radius, knn = self.find_circle_boundary(point, self.root, k)
+        radius, knn = self.update_knn(self.root, [], k, point, radius)
+        return knn
+
+    def query(self, xs: np.ndarray, k=1, return_distance=True):
+        if self.root.points_number >= k:
+            result = [self.find_knn(x, k) for x in xs]
+        else:
+            result = [self.root.points for _ in xs]
+        if return_distance:
+            return [[(p.id, p.get_dist(xs[i])) for p in knn] for i, knn in enumerate(result)]
+        else:
+            return [[p.id for p in knn] for knn in result]
---
+++ hw01/src/utils.py
@@ -0,0 +1,62 @@
+import random
+import typing
+
+import pandas
+import numpy as np
+
+
+def read_cancer_dataset(path_to_csv: str) -> typing.Tuple[typing.List, typing.List]:
+    data = pandas.read_csv(path_to_csv).values.tolist()
+    random.shuffle(data)
+    return [row[1:] for row in data], [(1 if row[0] == 'M' else 0) for row in data]
+
+
+def read_spam_dataset(path_to_csv: str) -> typing.Tuple[typing.List, typing.List]:
+    data = pandas.read_csv(path_to_csv).values.tolist()
+    random.shuffle(data)
+    return [row[:-1] for row in data], [int(row[-1]) for row in data]
+
+
+def train_test_split(
+        xs: typing.List,
+        ys: typing.List,
+        ratio: float
+) -> typing.Tuple[typing.List, typing.List, typing.List, typing.List]:
+    assert len(xs) == len(ys)
+    train_size = int(ratio * len(xs))
+    return xs[:train_size], ys[:train_size], xs[train_size:], ys[train_size:]
+
+
+def get_precision_recall_accuracy(y_pred: list, y_true: list) -> typing.Tuple[typing.List, typing.List, float]:
+    assert len(y_pred) == len(y_true)
+    n = len(y_pred)
+    assert n > 0
+
+    accuracy = sum(1 for yp, yt in zip(y_pred, y_true) if yp == yt) / n
+    classes = set(y_true + y_pred)
+    cn = len(classes)
+
+    precision = [0.0] * cn
+    recall = [0.0] * cn
+
+    for i, y in enumerate(classes):
+        tp = sum(1 for yp, yt in zip(y_pred, y_true) if yp == y and yt == y)  # true positive
+        fp = sum(1 for yp, yt in zip(y_pred, y_true) if yp == y and yt != y)  # false positive
+        fn = sum(1 for yp, yt in zip(y_pred, y_true) if yp != y and yt == y)  # false negative
+        precision[i] = tp / (tp + fp)
+        recall[i] = tp / (tp + fn)
+
+    return precision, recall, accuracy
+
+
+def standard_scale(xs: np.ndarray, mean: np.ndarray, std: np.ndarray):
+    return (xs - mean) / std
+
+
+# scale [min, max] to [0, 1]
+def zero_one_scale(xs: np.ndarray, _min: np.ndarray, _max: np.ndarray):
+    return (xs - _min) / (_max - _min)
+
+
+def max_abs_scale(xs: np.ndarray, _max_abs: np.ndarray):
+    return xs / _max_abs
