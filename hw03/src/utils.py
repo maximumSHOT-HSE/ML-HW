d62dca246c8527327f6337783a966cc7f731bfa6
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-09-27 12:31:35 +0300

    add 1,2 tasks

---
+++ hw03/src/utils.py
@@ -0,0 +1,35 @@
+from sklearn.datasets import make_blobs, make_moons
+import numpy as np
+import pandas
+import random
+import matplotlib.pyplot as plt
+import matplotlib
+import typing
+import math
+
+
+def calc_probabilities(labels: np.ndarray) -> np.ndarray:
+    n_labels = labels.shape[0]
+    occur = dict()
+    for label in labels:
+        if label in occur:
+            occur[label] += 1
+        else:
+            occur[label] = 1
+    return np.array([occ / n_labels for occ in occur.values()])
+
+
+def gini(labels: np.ndarray):
+    return sum(p * (1 - p) for p in calc_probabilities(labels))
+
+
+def entropy(labels: np.ndarray):
+    return -sum(p * math.log(p) for p in calc_probabilities(labels))
+
+
+def gain(left_y: np.ndarray, right_y: np.ndarray, criterion):
+    left_size = left_y.shape[0]
+    right_size = right_y.shape[0]
+    return (left_size + right_size) * criterion(np.concatenate(left_y, right_y)) - \
+           (left_size * criterion(left_y)) - \
+           (right_size * criterion(right_y))
