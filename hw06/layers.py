6a5d60b9962d616b3bdb7d6fa1c4abcfc4892fe1
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-11-09 22:33:30 +0300

    add backward and update for linear layer

--- hw06/layers.py
+++ hw06/layers.py
@@ -18,17 +18,21 @@ class Module:
 class Linear(Module):
     def __init__(self, in_features, out_features):
         self.W = np.random.uniform(-1, 1, size=(out_features, in_features + 1))
+        self.x = None
+        self.y = None
 
     def forward(self, X):
         x = X if len(X.shape) > 1 else X.reshape(-1, 1)
         x = np.vstack((x, np.ones((1, x.shape[1]))))
-        return self.W.dot(x)
+        self.x = x
+        self.y = self.W.dot(x)
+        return self.y
 
     def backward(self, d):
-        pass
+        return self.W.T.dot(d.reshape(-1, 1))
 
     def update(self, alpha):
-        pass
+        self.W -= alpha * (self.y.dot(self.x.T))
 
 
 class ReLU(Module):
